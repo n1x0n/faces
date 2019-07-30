efa7404b8e52143a63a4dd9dcdead921b53f38b5
Fredrik Nygren <fredrik@phight.club>
2019-07-30 17:14:02 +0200

    flask

--- app/run.py
+++ app/run.py
@@ -1,1 +1,6 @@
-# Nothing to see here yet
+from faces import app
+import os
+
+app.secret_key = os.urandom(24)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
