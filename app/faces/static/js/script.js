45718457b73e42edc0dcaec3556acb035c8876d4
Fredrik Nygren <fredrik@phight.club>
2019-07-31 20:04:14 +0200

    fn

--- app/faces/static/js/script.js
+++ app/faces/static/js/script.js
@@ -61,9 +61,9 @@ $(document).ready(function() {
     });
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-        var currentTab = $(e.target).text();
+        var currentTab = $(e.target).attr("id");
         console.log(currentTab);
-        if (currentTab == "photo_library Existing photos") {
+        if (currentTab == "existing") {
             alert("On existing");
         }
     });
