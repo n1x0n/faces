1d90c8014e37272ddcab67b13f629230b7dee618
Fredrik Nygren <fredrik@phight.club>
2019-07-31 19:59:20 +0200

    fn

--- app/faces/static/js/script.js
+++ app/faces/static/js/script.js
@@ -60,6 +60,13 @@ $(document).ready(function() {
         });
     });
 
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        var currentTab = $(e.target).text(); // get current tab
+        alert(currentTab);
+    });
+
+
+
 });
 
 
